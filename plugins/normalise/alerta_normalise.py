654ce649fe279de575742a2f51cd20467f6e4b37
Neil Orley <neilorley@gmail.com>
2022-02-02 11:29:29 +0100

    Normalise "text", "resource" and "customer" attribute.
    Catches base URL in resource attribute
    Catches IP address in resource attribute
    Gives alerts to customer MCO by default
    Removes criticity to alert text

--- plugins/normalise/alerta_normalise.py
+++ plugins/normalise/alerta_normalise.py
@@ -5,7 +5,6 @@ from alerta.plugins import PluginBase
 
 LOG = logging.getLogger('alerta.plugins.normalise')
 
-
 class NormaliseAlert(PluginBase):
 
     def pre_receive(self, alert):
@@ -13,18 +12,40 @@ class NormaliseAlert(PluginBase):
         LOG.info("Normalising alert...")
 
         # prepend severity to alert text
-        alert.text = '%s: %s' % (alert.severity.upper(), alert.text)
+        #alert.text = '%s: %s' % (alert.severity.upper(), alert.text)
 
         # supply different default values if missing
         if not alert.group or alert.group == 'Misc':
             alert.group = 'Unknown'
         if not alert.value or alert.value == 'n/a':
             alert.value = '--'
+        if not alert.customer or alert.customer == 'n/a' or alert.customer == 'alerta-mco':
+            alert.customer = 'MCO'
+
+
+        # normalise resource
+        baseurl = re.match("http[s]?://([^/?/$|^:?]+)",alert.resource)
+        ip = re.match("([0-9\.]+)",alert.resource)
+        if baseurl:
+            alert.resource = baseurl.group(0)
+        elif ip:
+            alert.resource = ip.group(0)
+
 
         return alert
 
     def post_receive(self, alert):
-        return
+
+        # normalise resource
+        baseurl = re.match("http[s]?://([^/?/$|^:?]+)",alert.resource)
+        ip = re.match("([0-9\.]+)",alert.resource)
+        if baseurl:
+            alert.resource = baseurl.group(0)
+        elif ip:
+            alert.resource = ip.group(0)
+
+        return alert
 
     def status_change(self, alert, status, text):
         return
+
