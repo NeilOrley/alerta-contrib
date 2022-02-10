4cd540f789b6dd35448f265e6ddb059052c6319f
Neil Orley <neilorley@gmail.com>
2022-02-10 16:55:34 +0100

    Change the alert description
    Change the alert description using a pattern : severity: group "is" event "on" resource

--- plugins/normalise/alerta_normalise.py
+++ plugins/normalise/alerta_normalise.py
@@ -1,5 +1,6 @@
 
 import logging
+import re
 
 from alerta.plugins import PluginBase
 
@@ -13,13 +14,36 @@ class NormaliseAlert(PluginBase):
         LOG.info("Normalising alert...")
 
         # prepend severity to alert text
-        alert.text = '%s: %s' % (alert.severity.upper(), alert.text)
+        alert.text = '%s: %s is %s on %s' % (alert.severity.upper(), alert.group, alert.event, alert.resource)
 
         # supply different default values if missing
         if not alert.group or alert.group == 'Misc':
             alert.group = 'Unknown'
         if not alert.value or alert.value == 'n/a':
             alert.value = '--'
+        if not alert.customer or alert.customer == 'n/a' or alert.customer == 'alerta-mco':
+            alert.customer = 'MCO'
+
+        # normalise group
+        if re.search("exportermissing|exporter_down|cloudprober_down|ExporterFlapping|Cloudprober_Down",alert.event,re.IGNORECASE):
+            alert.group = 'Monitoring'
+        elif re.search("disk|inode",alert.event,re.IGNORECASE):
+            alert.group = 'Filesystem'
+        elif re.search("Blackbox_exporter_Lattency|docker|supervisor",alert.event,re.IGNORECASE):
+            alert.group = 'Service'
+        elif re.search("DB_Oracle_Not_Responding",alert.event,re.IGNORECASE):
+            alert.group = 'DB'
+        elif re.search("idrac",alert.event,re.IGNORECASE):
+            alert.group = 'Hardware'
+
+        # normalise resource
+        baseurl = re.match("http[s]?://([^/?/$|^:?]+)",alert.resource)
+        ip = re.match("([0-9\.]+)",alert.resource)
+        if baseurl:
+            alert.resource = baseurl.group(0)
+        elif ip:
+            alert.resource = ip.group(0)
+
 
         return alert
 
